2282b694c0d833afed0415223726018a0ec2493f
Danielle Foss <danifoss2011@gmail.com>
2023-08-20 11:19:07 -0600

    file restructure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+    return (
+        <div>
+            <header className='header'>
+                <div>Logo</div>
+                <div>Date</div>
+                <div>
+                    <button type='text'></button>
+                </div>
+                <div>Name</div>
+                <div>Avatar</div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
