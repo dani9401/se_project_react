f718efc9f997921653b8f3c6586263e66a268176
Danielle Foss <danifoss2011@gmail.com>
2023-08-21 15:02:17 -0600

    add main and utils folders and destructured app

---
+++ src/Main/Main.js
@@ -0,0 +1,17 @@
+import { defaultClothingItems } from "../utils/constants";
+import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+
+function Main({weatherTemp}) {
+    return <main className="main">
+        <WeatherCard day={false} type='storm' weatherTemp={weatherTemp} />
+        <section className="card__section" id='card'>
+            Today is {weatherTemp} / You may want to wear:
+            <div className="card__items"> {defaultClothingItems.map((item) => (
+                <ItemCard item={item} />
+            ))}</div>
+        </section>
+    </main>;
+}
+
+export default Main;
