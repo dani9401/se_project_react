8d327799dd56616d59e1922f2ce242c4c4c308db
Danielle Foss <danifoss2011@gmail.com>
2023-08-21 19:53:41 -0600

    add styles for card__section-title

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -7,7 +7,7 @@ function Main({weatherTemp}) {
     return <main className="main">
         <WeatherCard day={false} type='storm' weatherTemp={weatherTemp} />
         <section className="card__section" id='card'>
-            Today is {weatherTemp} / You may want to wear:
+            <h2>Today is {weatherTemp} / You may want to wear:</h2>
             <div className="card__items"> {defaultClothingItems.map((item) => (
                 <ItemCard item={item} />
             ))}</div>
