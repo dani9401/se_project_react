27986f3ae88f8fac6b5fc769a32532937c6d042f
Danielle Foss <danifoss2011@gmail.com>
2023-08-23 12:57:59 -0600

    add weatherType and logic for filtering cards on weatherType

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,13 +2,31 @@ import { defaultClothingItems } from "../utils/constants";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import './Main.css'
+import { useMemo } from "react";
 
 function Main({weatherTemp, onSelectCard}) {
+
+    const weatherType = useMemo(() => {
+        if (weatherTemp >= 86) {
+            return 'hot';
+          } else if (weatherTemp >= 66 && weatherTemp <= 85) {
+            return 'warm';
+          } else if (weatherTemp <= 65) {
+            return 'cold';
+          }
+    }, [weatherTemp]) //dependencies go in brackets
+
+    const filteredCards = defaultClothingItems.filter((item) => {
+        console.log(item);
+        return item.weather.toLowerCase() === weatherType; //toLowerCase removes possibility that card might have weather style in non-identical string, ie; Hot vs hot
+    })
+    console.log(filteredCards)
+
     return <main className="main">
         <WeatherCard day={false} type='storm' weatherTemp={weatherTemp} />
         <section className="card__section" id='card'>
             <h2 className="card__section-title">Today is {weatherTemp}°F / You may want to wear:</h2>
-            <div className="card__items"> {defaultClothingItems.map((item) => (
+            <div className="card__items"> {filteredCards.map((item) => (
                 <ItemCard item={item} onSelectCard={onSelectCard}/>
             ))}</div>
         </section>
