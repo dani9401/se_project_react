ee69c9b83749a309fb6dd4b75fd48c420d4eeeaa
Danielle Foss <danifoss2011@gmail.com>
2023-08-23 14:33:46 -0600

    fix bug on className

--- src/ItemCard/ItemCard.js
+++ src/ItemCard/ItemCard.js
@@ -2,7 +2,7 @@ import './ItemCard.css'
 
 const ItemCard = ({item, onSelectCard}) => {
     return (
-        <div classname="card__element">
+        <div className="card__element">
                 <img src={item.link} className="card__image" onClick={() => onSelectCard(item)}/>
 
             <h2 className="card__name">{item.name}</h2>
