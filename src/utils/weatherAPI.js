9c277cb33003da54c40ee4aa2be21a9a3a3b79d9
Danielle Foss <danifoss2011@gmail.com>
2023-08-23 14:08:29 -0600

    update default long and latt values for testing

--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -1,8 +1,8 @@
 //https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}
 //My API Key: 68247c01732e1119e845b344423d9160
 
-const latitude = 44.34;
-const longitude = 10.99;
+const latitude = 39.73;
+const longitude = -104.99;
 const apiKey = "68247c01732e1119e845b344423d9160";
 
 export const getWeatherForecast = () => {
