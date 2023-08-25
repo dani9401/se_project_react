98d4645aeab28b29d073fd40346f0e62ab064fa4
Danielle Foss <danifoss2011@gmail.com>
2023-08-25 13:28:08 -0600

    add new classes and styling

--- src/ItemModal/ItemModal.js
+++ src/ItemModal/ItemModal.js
@@ -1,12 +1,15 @@
+import '../ModalWithForm/ModalWithForm.css'
+import "./ItemModal.css"
+
 const ItemModal = ({selectedCard, onClose}) => {
     console.log("item modal")
     return (
         <div className={`modal`}>
-        <div className="modal__content">
-        <button type="button" onClick={onClose} className="modal__close-button">Close</button>
-        <img src={selectedCard.link}></img>
-        <div>{selectedCard.name}</div>
-        <div>Weather Type: {selectedCard.weather}</div>
+        <div className="modal__container modal__container-image">
+        <button type="button" onClick={onClose} className="modal__close-button"></button>
+        <img src={selectedCard.link} className="modal__image-preview"></img>
+        <div className="modal__item-name">{selectedCard.name}</div>
+        <div className="modal__weather-type">Weather Type: {selectedCard.weather}</div>
         </div>
     </div>
     )
