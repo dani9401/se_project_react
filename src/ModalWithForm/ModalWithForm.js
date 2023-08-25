239b76424dccfcb515e48287d087d15805f6953a
Danielle Foss <danifoss2011@gmail.com>
2023-08-25 13:29:10 -0600

    update classes and add styling

--- src/ModalWithForm/ModalWithForm.js
+++ src/ModalWithForm/ModalWithForm.js
@@ -1,11 +1,10 @@
 import './ModalWithForm.css'
 
 const ModalWithForm = ({children, buttonText = 'Add Garment', title, onClose, modalName}) => {
-    console.log("ModalWithForm")
     return (
     <div className={`modal modal-type-${modalName}`}>
-        <div className="modal__content">
-        <button type="button" onClick={onClose} className="modal__close-button">Close</button>
+        <div className="modal__container">
+        <button type="button" onClick={onClose} className="modal__close-button"></button>
         <h3 className="modal__title">{title}</h3>
         <form className="modal__form">{children}</form>
         <button type="submit" className="modal__submit-button">{buttonText}</button>
