f1207d239e9e3c21c466f26fd51eff5784822480
Danielle Foss <danifoss2011@gmail.com>
2023-08-27 14:35:52 -0600

    update modal close button for ItemModal

--- src/ItemModal/ItemModal.js
+++ src/ItemModal/ItemModal.js
@@ -6,7 +6,7 @@ const ItemModal = ({selectedCard, onClose}) => {
     return (
         <div className={`modal`}>
         <div className="modal__container modal__container-image">
-        <button type="button" onClick={onClose} className="modal__close-button"></button>
+        <button type="button" onClick={onClose} className="modal__close-button modal__close-button-white"></button>
         <img src={selectedCard.link} className="modal__image-preview"></img>
         <div className="modal__item-name">{selectedCard.name}</div>
         <div className="modal__weather-type">Weather Type: {selectedCard.weather}</div>
