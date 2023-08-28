1a7f9d31b03511a986568fe6f8e6e8861ba1eb58
Danielle Foss <danifoss2011@gmail.com>
2023-08-28 13:35:05 -0600

    move submit button to be included in form tags

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -18,9 +18,9 @@ const ModalWithForm = ({
         <h3 className="modal__title">{title}</h3>
         <form className="modal__form">
           {children}
-        <button type="submit" className="modal__submit-button">
-          {buttonText}
-        </button>
+          <button type="submit" className="modal__submit-button">
+            {buttonText}
+          </button>
         </form>
       </div>
     </div>
