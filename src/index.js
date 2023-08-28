79b1b7d12560609e1befcde3631b5644c6ef6a2d
Danielle Foss <danifoss2011@gmail.com>
2023-08-27 20:46:52 -0600

    clean up unneeded imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "../src/App/App.css"
 import App from './App/App.js';
 import reportWebVitals from './reportWebVitals';
 
