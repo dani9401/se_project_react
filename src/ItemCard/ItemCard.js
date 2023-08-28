5860f11dbb1550ff910ce386c0554330fa89902e
Danielle Foss <danifoss2011@gmail.com>
2023-08-28 08:31:11 -0600

    clean up unneeded code

--- src/ItemCard/ItemCard.js
+++ src/ItemCard/ItemCard.js
@@ -2,7 +2,7 @@ import './ItemCard.css'
 
 const ItemCard = ({item, onSelectCard}) => {
     return (
-        <div className="card__element" key={item._id}>
+        <div className="card__element">
                 <img src={item.link} className="card__image" onClick={() => onSelectCard(item)}/>
 
             <h2 className="card__name">{item.name}</h2>
