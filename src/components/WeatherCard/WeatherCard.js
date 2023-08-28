0724d3133899af4cf311e18cfaa18396e9ec6949
Danielle Foss <danifoss2011@gmail.com>
2023-08-28 13:35:28 -0600

    move weatherOptions to constants.js

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,67 +1,6 @@
 import "../WeatherCard/WeatherCard.css";
+import { weatherOptions } from "../../utils/constants";
 
-const weatherOptions = [
-  {
-    url: require("../../images/Day/Day - Sunny.svg").default,
-    day: true,
-    type: "sunny",
-  },
-  {
-    url: require("../../images/Day/Day - Cloudy.svg").default,
-    day: true,
-    type: "cloudy",
-  },
-  {
-    url: require("../../images/Day/Day - Fog.svg").default,
-    day: true,
-    type: "fog",
-  },
-  {
-    url: require("../../images/Day/Day - Rain.svg").default,
-    day: true,
-    type: "rain",
-  },
-  {
-    url: require("../../images/Day/Day - Snow.svg").default,
-    day: true,
-    type: "snow",
-  },
-  {
-    url: require("../../images/Day/Day - Storm.svg").default,
-    day: true,
-    type: "storm",
-  },
-  {
-    url: require("../../images/Night/Night - Clear.svg").default,
-    day: false,
-    type: "clear",
-  },
-  {
-    url: require("../../images/Night/Night - Cloudy.svg").default,
-    day: false,
-    type: "cloudy",
-  },
-  {
-    url: require("../../images/Night/Night - Fog.svg").default,
-    day: false,
-    type: "fog",
-  },
-  {
-    url: require("../../images/Night/Night - Rain.svg").default,
-    day: false,
-    type: "rain",
-  },
-  {
-    url: require("../../images/Night/Night - Snow.svg").default,
-    day: false,
-    type: "snow",
-  },
-  {
-    url: require("../../images/Night/Night - Storm.svg").default,
-    day: false,
-    type: "storm",
-  },
-];
 
 const WeatherCard = ({ day, type, weatherTemp = "" }) => {
   const imageSource = weatherOptions.filter((i) => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -40,3 +40,66 @@ export const defaultClothingItems = [
 export const latitude = 39.73;
 export const longitude = -104.99;
 export const apiKey = "68247c01732e1119e845b344423d9160";
+
+export const weatherOptions = [
+  {
+    url: require("../images/Day/Day - Sunny.svg").default,
+    day: true,
+    type: "sunny",
+  },
+  {
+    url: require("../images/Day/Day - Cloudy.svg").default,
+    day: true,
+    type: "cloudy",
+  },
+  {
+    url: require("../images/Day/Day - Fog.svg").default,
+    day: true,
+    type: "fog",
+  },
+  {
+    url: require("../images/Day/Day - Rain.svg").default,
+    day: true,
+    type: "rain",
+  },
+  {
+    url: require("../images/Day/Day - Snow.svg").default,
+    day: true,
+    type: "snow",
+  },
+  {
+    url: require("../images/Day/Day - Storm.svg").default,
+    day: true,
+    type: "storm",
+  },
+  {
+    url: require("../images/Night/Night - Clear.svg").default,
+    day: false,
+    type: "clear",
+  },
+  {
+    url: require("../images/Night/Night - Cloudy.svg").default,
+    day: false,
+    type: "cloudy",
+  },
+  {
+    url: require("../images/Night/Night - Fog.svg").default,
+    day: false,
+    type: "fog",
+  },
+  {
+    url: require("../images/Night/Night - Rain.svg").default,
+    day: false,
+    type: "rain",
+  },
+  {
+    url: require("../images/Night/Night - Snow.svg").default,
+    day: false,
+    type: "snow",
+  },
+  {
+    url: require("../images/Night/Night - Storm.svg").default,
+    day: false,
+    type: "storm",
+  },
+];
