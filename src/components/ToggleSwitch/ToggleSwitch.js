66cf51434c5e9b56253dc4788309481d4ab4bfc7
Danielle Foss <danifoss2011@gmail.com>
2023-08-30 22:01:39 -0600

    add ToggleSwitch styles

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ToggleSwitch.css";
 
 const ToggleSwitch = () => {
-  return <div>Toggle Switch</div>;
-};
+  const [currentTemperatureUnit, handleToggleSwitchChange] = useState("C");
 
-ToggleSwitch();
+  const handleChange = () => {
+    if (currentTemperatureUnit === "C") handleToggleSwitchChange("F");
+    if (currentTemperatureUnit === "F") handleToggleSwitchChange("C");
+  };
+
+  console.log(currentTemperatureUnit);
+
+  return (
+    <label className="switch">
+      <input
+        type="checkbox"
+        className="switch__box"
+        onChange={handleChange}
+      ></input>
+      <span
+        className={
+          currentTemperatureUnit === "F"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider switch__slider-C"
+        }
+      ></span>
+      <p
+        className={`switch__temp-f ${
+          currentTemperatureUnit === "F" && "switch__active"
+        }`}
+      >
+        F
+      </p>
+      <p
+        className={`switch__temp-c ${
+          currentTemperatureUnit === "C" && "switch__active"
+        }`}
+      >
+        C
+      </p>
+    </label>
+  );
+};
 
 export default ToggleSwitch;
