0804b6d958f9e88a5dc9f0dfa95de54b224a07c6
Danielle Foss <danifoss2011@gmail.com>
2023-09-02 09:03:57 -0600

    add nav links to logo and name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,16 @@ import "./Header.css";
 import Logo from "../../images/Logo_wtwr.svg";
 import Avatar from "../../images/avatar.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Header = ({ onCreateModal, weatherLocation }) => {
   return (
     <header className="header">
       <div className="header__menu-left">
         <div className="header__logo">
-          <img src={Logo} alt="WTWR-logo"></img>
+          <Link to="/">
+            <img src={Logo} alt="WTWR-logo"></img>
+          </Link>
         </div>
         <h3 className="header__date-location">August 23, {weatherLocation}</h3>
       </div>
@@ -23,7 +26,9 @@ const Header = ({ onCreateModal, weatherLocation }) => {
             + Add Clothes
           </button>
         </div>
-        <h3 className="header__name">Danielle Foss</h3>
+        <Link className="header__name" to="/profile">
+          Danielle Foss
+        </Link>
         <div>
           <img src={Avatar} alt="avatar" className="header__avatar"></img>
         </div>
