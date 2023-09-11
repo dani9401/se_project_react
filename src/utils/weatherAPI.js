9ede407a08698c41a5cea7eec03e004c888c833c
Danielle Foss <danifoss2011@gmail.com>
2023-09-11 08:17:24 -0600

    shorten code with checkResponse method

--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -1,15 +1,10 @@
 import { apiKey, latitude, longitude } from "./constants";
+import { checkResponse } from "./utils";
 
 export const getWeatherForecast = () => {
   const weatherApi = fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`
-  ).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error`);
-    }
-  });
+  ).then(checkResponse);
   return weatherApi;
 };
 
