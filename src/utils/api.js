6f7d864933bd92391091b6b537a6cf7a5ada8965
Danielle Foss <danifoss2011@gmail.com>
2023-10-30 14:54:25 -0600

    add signup and signin

--- src/utils/api.js
+++ src/utils/api.js
@@ -32,3 +32,23 @@ export const deleteClothingItems = (id) => {
     },
   }).then(checkResponse);
 };
+
+export const postSignup = ({ name, avatar, email, password }) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar, email, password }),
+  }).then(checkResponse);
+};
+
+export const postSignIn = ({ email, password }) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+};
