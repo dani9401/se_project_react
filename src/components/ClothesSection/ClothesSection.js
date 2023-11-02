91cc0afa7b75caf76d9b638c23e66d9fda2fe460
Danielle Foss <danifoss2011@gmail.com>
2023-11-01 21:15:43 -0600

    add starting code for comparing clothingItemId to ownerId

--- src/components/ClothesSection/ClothesSection.js
+++ src/components/ClothesSection/ClothesSection.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ItemCard from "../ItemCard/ItemCard";
 
-const ClothesSection = ({ onCreateModal, clothingItems, onSelectCard }) => {
+const ClothesSection = ({ onCreateModal, clothingItems, onSelectCard, currentUser }) => {
+
+  //get list of clothingItemIds
+const clothingItemId = clothingItems.map((item) => {
+  return item.id;
+});
+
+
+ // Checking if the current user is the owner of the current clothing item
+//const isOwner = clothingItemId.owner.id === currentUser.id;
+
+//return only clothing items that belong to that owner - create new array with approved 
+//items and pass to map below
+
+
+
   return (
     <div className="profile__clothes-section">
       <div className="profile__menu-top">
