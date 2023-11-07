5776f01da25a0efe8b8780363e5ee85a8b64261b
Danielle Foss <danifoss2011@gmail.com>
2023-11-07 16:01:02 -0700

    add editProfileModal props

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,11 +3,16 @@ import "./Profile.css";
 import SideBar from "../SideBar/SideBar";
 import ClothesSection from "../ClothesSection/ClothesSection";
 
-const Profile = ({ onCreateModal, clothingItems, onSelectCard }) => {
+const Profile = ({
+  onCreateModal,
+  clothingItems,
+  onSelectCard,
+  onEditProfileModal,
+}) => {
   return (
     <section className="profile">
       <div className="profile__sidebar">
-        <SideBar />
+        <SideBar onEditProfileModal={onEditProfileModal} />
       </div>
       <ClothesSection
         onCreateModal={onCreateModal}
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -2,19 +2,30 @@ import React, { useContext } from "react";
 import Avatar from "../../images/avatar.svg";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-const SideBar = () => {
-
-  const currentUser = useContext(CurrentUserContext)
+const SideBar = ({ onEditProfileModal }) => {
+  const currentUser = useContext(CurrentUserContext);
 
   return (
     <div className="profile__sidebar">
       <div className="profile__sidebar-avatar-name">
-      <img className="profile__avatar" src={currentUser.avatar} alt="avatar"></img>
-      <div className="profile__name">{currentUser.name}</div>
+        <img
+          className="profile__avatar"
+          src={currentUser.avatar}
+          alt="avatar"
+        ></img>
+        <div className="profile__name">{currentUser.name}</div>
       </div>
       <div className="profile__sidebar-button-container">
-      <button className="profile__sidebar-button" type="button">Change Profile Data</button>
-      <button className="profile__sidebar-button" type="button">Log Out</button>
+        <button
+          className="profile__sidebar-button"
+          type="button"
+          onClick={onEditProfileModal}
+        >
+          Change Profile Data
+        </button>
+        <button className="profile__sidebar-button" type="button">
+          Log Out
+        </button>
       </div>
     </div>
   );
