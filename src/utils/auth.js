680576c4f2507189c6155295c3246fedaeaef721
Danielle Foss <danifoss2011@gmail.com>
2023-11-07 20:26:13 -0700

    add editProfile and prettier format

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,36 +3,47 @@ import { checkResponse } from "./utils";
 
 //sign up
 export const postSignup = ({ email, password, name, avatar }) => {
-    return fetch(`${baseUrl}/signup`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password, name, avatar }),
-    }).then(checkResponse);
-  };
-  
-  //sign in
-  export const postSignIn = ({ email, password }) => {
-    return fetch(`${baseUrl}/signin`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    }).then(checkResponse);
-  };
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, name, avatar }),
+  }).then(checkResponse);
+};
 
-  //get user info
- export const getUserInfo = (token) => {
-    return fetch(`${baseUrl}/users/me`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: `Bearer ${token}`,
-      },
-    }).then(checkResponse);
-  };
+//sign in
+export const postSignIn = ({ email, password }) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+};
 
+//get user info
+export const getUserInfo = (token) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
+};
+
+//edit profile
+export const editProfile = (name, avatar, token) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ name, avatar }),
+  }).then(checkResponse);
+};
