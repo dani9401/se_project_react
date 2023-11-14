44279d15663985b862d90ce85192de29d27b4c96
Danielle Foss <danifoss2011@gmail.com>
2023-11-13 19:22:09 -0700

    update props and handleRegisterClick

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -1,48 +1,47 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import ModalWithForm from "../ModalWithForm/ModalWithForm"
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
-const LoginModal = ({handleCloseModal, isOpen, onLogin}) => {
-    //think about what props I need for this to function
+const LoginModal = ({ handleCloseModal, isOpen, onLogin, setActiveModal }) => {
+  //think about what props I need for this to function
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    useEffect(() => {
-        if (!isOpen) {
-          setEmail("");
-          setPassword("");
-        }
-      }, [isOpen]);
+  useEffect(() => {
+    if (!isOpen) {
+      setEmail("");
+      setPassword("");
+    }
+  }, [isOpen]);
 
-      //handlers go here
+  //handlers go here
 
-      const handleLoginSubmit = (e) => {
-        e.preventDefault();
-        onLogin(email, password)
-      };
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    onLogin(email, password);
+  };
 
-      const handleRegisterClick = () => {
-              //close Login modal
-              //open Register modal
-      }
+  const handleRegisterClick = (e) => {
+    setActiveModal("register");
+  };
 
-      const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-      }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-      const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-      }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    return (
-        <ModalWithForm 
-        title="Log In" 
-        onClose={handleCloseModal} 
-        isOpen={isOpen} 
-        modalName={"login"}>
-
-        <div className="modal__text-inputs">
+  return (
+    <ModalWithForm
+      title="Log In"
+      onClose={handleCloseModal}
+      isOpen={isOpen}
+      modalName={"login"}
+    >
+      <div className="modal__text-inputs">
         <label className="modal__label">
           Email
           <input
@@ -70,17 +69,24 @@ const LoginModal = ({handleCloseModal, isOpen, onLogin}) => {
           ></input>
         </label>
       </div>
-      <div> 
-      <button type="submit" className="modal__submit-button modal__submit-button1" onClick={handleLoginSubmit}>
-            Log In
-          </button>
-          <button type="submit" className="modal__submit-button modal__submit-button2" onClick={handleRegisterClick}>
-            or Register
-          </button>
-        </div> 
-        </ModalWithForm>
-        
-    )
-}
+      <div>
+        <button
+          type="submit"
+          className="modal__submit-button modal__submit-button1"
+          onClick={handleLoginSubmit}
+        >
+          Log In
+        </button>
+        <button
+          type="button"
+          className="modal__submit-button modal__submit-button2"
+          onClick={handleRegisterClick}
+        >
+          or Register
+        </button>
+      </div>
+    </ModalWithForm>
+  );
+};
 
 export default LoginModal;
