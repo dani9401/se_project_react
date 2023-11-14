921abb63e11acbd3620445e0239dfb59e87d7017
Danielle Foss <danifoss2011@gmail.com>
2023-11-13 19:23:16 -0700

    add showAvatar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,17 @@ import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-const Header = ({ onCreateModal, weatherLocation, onLoginModal, onRegisterModal, loggedIn }) => {
-
-const currentUser = useContext(CurrentUserContext)
-const avatarImage = currentUser.avatar;
+const Header = ({
+  onCreateModal,
+  weatherLocation,
+  onLoginModal,
+  onRegisterModal,
+  loggedIn,
+}) => {
+  const currentUser = useContext(CurrentUserContext);
+  const avatar = currentUser ? currentUser.avatar : undefined;
+  const showAvatar = avatar !== "" ? true : false;
+  const name = currentUser.name;
 
   return (
     <header className="header">
@@ -25,44 +32,50 @@ const avatarImage = currentUser.avatar;
         <h3 className="header__date-location">August 23, {weatherLocation}</h3>
       </div>
       {loggedIn ? (
-      <div className="header__menu-right">
-        <ToggleSwitch />
-        <div className="header__menu-buttons">
-          <button
-            className="header__add-button"
-            onClick={onCreateModal}
-            type="text"
-          >
-            + Add Clothes
-          </button>
+        <div className="header__menu-right">
+          <ToggleSwitch />
+          <div className="header__menu-buttons">
+            <button
+              className="header__add-button"
+              onClick={onCreateModal}
+              type="text"
+            >
+              + Add Clothes
+            </button>
+          </div>
+          <Link className="header__name" to="/profile">
+            {currentUser.name}
+          </Link>
+          <div>
+            {showAvatar ? (
+              <img className="header__avatar" src={avatar} alt="avatar" />
+            ) : (
+              <p className="header__avatar-placeholder">
+                {name[0]?.toUpperCase()}
+              </p>
+            )}
+          </div>
         </div>
-        <Link className="header__name" to="/profile">
-          {currentUser.name}
-        </Link>
-        <div>
-          <img src={avatarImage} alt="avatar" className="header__avatar"></img>
-        </div>
-      </div>
       ) : (
         <div className="header__menu-right">
-        <ToggleSwitch />
-        <div className="header__menu-buttons">
-          <button
-            className="header__add-button"
-            onClick={onRegisterModal}
-            type="text"
-          >
-            Sign Up
-          </button>
-          <button
-            className="header__add-button"
-            onClick={onLoginModal}
-            type="text"
-          >
-            Log In
-          </button>
+          <ToggleSwitch />
+          <div className="header__menu-buttons">
+            <button
+              className="header__add-button"
+              onClick={onRegisterModal}
+              type="text"
+            >
+              Sign Up
+            </button>
+            <button
+              className="header__add-button"
+              onClick={onLoginModal}
+              type="text"
+            >
+              Log In
+            </button>
+          </div>
         </div>
-      </div>
       )}
     </header>
   );
